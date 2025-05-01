240d6b4d3bcde67f95724689f3bd778af82edece
RENoMafex <schilling.malte@googlemail.com>
2025-05-02 00:36:54 +0200

    added some doxygen comments

--- src/grcpp.hpp
+++ src/grcpp.hpp
@@ -18,20 +18,38 @@
 #define GRCPP_VERSION "v0.0.1(pre-alpha)"
 #define DLOG(x) std::cout<<x<<std::endl;
 
-struct Grcpp_Options { //The options directly used by grcpp
-    bool help = false; //does print_help_message() need to be called?
-    bool err = false; //redirect stderr
-    bool out = false; //redirect stdout
+///\struct Grcpp_Options
+///\brief The options directly used by grcpp.
+///
+/// This struct holds the options, which determine, what grcpp should do.
+///\param help Boolean, determines, if the help needs to get displayed.
+///\param err Boolean, determines, if stderr should be colorized.
+///\param out Boolean, determines, if stdout should be colorized.
+///\param confname String, configfile name for colourize().
+///\param color String, colorization of output (auto, on, off)
+struct Grcpp_Options {
+    bool help = false;
+    bool err = false;
+    bool out = false;
 //  bool pty = false; //use pseutdoterminal
-    std::string confname = {}; //configfile name for grcat
-    std::string color = "auto"; //colorization of output (auto, on, off)
+    std::string confname = {};
+    std::string color = "auto";
 };
 
-//prints help message
+/// @brief Prints the help message.
+/// @param called_name Should be argv[0], defaults to "grcpp".
 void print_help_msg(std::string_view called_name = "grcpp");
-//fills an 'Grcpp_Options' struct with the parsed arguments
+
+/// @brief Initializes the options for grcpp.
+/// @param argc Just argc.
+/// @param argv Just argv[].
+/// @param grcpp_options The struct, that holds the options.
+/// @param other_options A vector<string> which holds the options for the called program.
 void init_program_options(int argc, char* argv[], Grcpp_Options &grcpp_options, std::vector<std::string> &other_options);
-//checks if an invalid option for the '--color' option is set
+
+/// @brief Checks if the check.color argument is valid.
+/// @param check The struct, in which the color argument should be checked.
+/// @return true, when invalid. false, when valid.
 bool invalid_color_arg(Grcpp_Options& check);
 
 #endif
