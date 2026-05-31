8248f32442373f9edc0ec20bef50f9208a6fb9c4
Malte Schilling <git@malte-schilling.eu>
2026-05-31 21:04:46 +0200

    make tests pass again

--- src/grcpp.hpp
+++ src/grcpp.hpp
@@ -28,13 +28,13 @@
 #include <vector>
 #include <thread>
 
-// #include <boost/regex.hpp>
-// #include <boost/program_options.hpp>
-// #include <boost/process.hpp>
+#include <boost/regex.hpp>
+#include <boost/program_options.hpp>
+#include <boost/process.hpp>
 
-#include "boost/include/boost/regex.hpp"
-#include "boost/include/boost/program_options.hpp"
-#include "boost/include/boost/process.hpp"
+// #include "boost/include/boost/regex.hpp"
+// #include "boost/include/boost/program_options.hpp"
+// #include "boost/include/boost/process.hpp"
 
 
 #include "colors.hpp"
